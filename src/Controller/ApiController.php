ccc056cd3d4f64661fd81fc1f7b960775b5c53d1
sega <hpsergeyy@yandex.ru>
2023-03-27 16:59:08 +0200

    Create ApiController.php

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api', name: 'api_')]
+class ApiController extends AbstractController
+{
+    #[Route('/test', name: 'test')]
+    public function test(): JsonResponse
+    {
+        return new JsonResponse();
+    }
+}
