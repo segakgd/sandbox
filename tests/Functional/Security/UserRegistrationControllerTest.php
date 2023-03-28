3c060386aa7d65e138125ef8ad394eaf66707ae1
sega <hpsergeyy@yandex.ru>
2023-03-28 16:16:17 +0200

    add lite test for UserRegistrationController

---
+++ tests/Functional/Security/UserRegistrationControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Functional\Security;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserRegistrationControllerTest extends WebTestCase
+{
+    public function testValidation()
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/api/user/registration/',
+            [],
+            [],
+            [],
+            '{"email":"test@user.com","password":"passwordTest"}'
+        );
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+    }
+}
