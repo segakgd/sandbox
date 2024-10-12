f04869825fb517abedce168e515183c04cfe573f
Sega <hpsergeyy@yandex.ru>
2024-10-12 02:45:31 +0200

    Update ApiController.php

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Segakgd\FlexbeApiClient\Dto\FlexbeApiClientDto;
+use Segakgd\FlexbeApiClient\FlexbeApiManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/api', name: 'api_')]
 class ApiController extends AbstractController
 {
+    public function __construct(private readonly FlexbeApiManager $flexbeApiManager)
+    {
+    }
+
     #[Route('/test', name: 'test')]
     public function test(): JsonResponse
     {
+        $domain = 'domain.com'; // Replace with your Flexbe domain
+        $apiKey = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; // Replace with your Flexbe API key
+
+        $clientFlexbeDto = new FlexbeApiClientDto($domain, $apiKey);
+
+        $leads = $this->flexbeApiManager->getLeads($clientFlexbeDto);
+
+        dd($leads);
+
         return new JsonResponse();
     }
 }
