3da7d0a0dee499253d9bd44473fc915659a9d0fe
Sega <hpsergeyy@yandex.ru>
2025-07-25 16:07:58 +0200

    Update ApiController.php

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,6 +3,12 @@
 namespace App\Controller;
 
 use Segakgd\FlexbeApiClient\Dto\FlexbeApiClientDto;
+use Segakgd\FlexbeApiClient\Exception\BadRequestException;
+use Segakgd\FlexbeApiClient\Exception\Http\InvalidApiKeyException;
+use Segakgd\FlexbeApiClient\Exception\Http\LimitExceededException;
+use Segakgd\FlexbeApiClient\Exception\Http\UndefinedActionException;
+use Segakgd\FlexbeApiClient\Exception\Http\UnknownErrorException;
+use Segakgd\FlexbeApiClient\Exception\InvalidMethodException;
 use Segakgd\FlexbeApiClient\FlexbeApiManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -11,6 +17,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiController extends AbstractController
 {
 
+    /**
+     * @throws UnknownErrorException
+     * @throws LimitExceededException
+     * @throws InvalidApiKeyException
+     * @throws BadRequestException
+     * @throws UndefinedActionException
+     * @throws InvalidMethodException
+     */
     #[Route('/', name: 'test')]
     public function test(): JsonResponse
     {
